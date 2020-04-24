6532b1040a5e310659e83dcfdca6f490805b93ae
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-24 15:12:02 +0200

    Name refactor

--- src/main/java/com/spring_rest_playground/demo/api/domain/Name.java
+++ src/main/java/com/spring_rest_playground/demo/api/domain/Name.java
@@ -1,48 +1,24 @@
 package com.spring_rest_playground.demo.api.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Name implements Serializable
 {
 
     private String title;
     private String first;
     private String last;
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private Map<String, Object> additionalProperties = new HashMap<>();
     private final static long serialVersionUID = 420620315591775395L;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getFirst() {
-        return first;
-    }
-
-    public void setFirst(String first) {
-        this.first = first;
-    }
-
-    public String getLast() {
-        return last;
-    }
-
-    public void setLast(String last) {
-        this.last = last;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
 }
