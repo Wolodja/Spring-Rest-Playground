0c51808054feaf5e9521ddd0ca1c19d69194f8b1
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-24 13:54:25 +0200

    Init version

---
+++ src/main/java/com/spring_rest_playground/demo/SpringRestPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.spring_rest_playground.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring_rest_playground/demo/SpringRestPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_rest_playground.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
