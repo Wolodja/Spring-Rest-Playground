5d198f3875ecf78896802105eb6d2b03fb410451
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-24 15:10:30 +0200

    Job refactor

--- src/main/java/com/spring_rest_playground/demo/api/domain/Job.java
+++ src/main/java/com/spring_rest_playground/demo/api/domain/Job.java
@@ -1,39 +1,22 @@
 package com.spring_rest_playground.demo.api.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class Job implements Serializable
 {
 
     private String title;
     private String company;
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private Map<String, Object> additionalProperties = new HashMap<>();
     private final static long serialVersionUID = -4985150429002262656L;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
 }
